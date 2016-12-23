920b3b1330bb99182fe9842158719168edfa5e83
Charlie Jackson <charlie@charliejackson.com>
2016-12-23 11:34:02 +0000

    Got rid of redundant package

--- scripts/create-droplet.php
+++ scripts/create-droplet.php
@@ -2,9 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
-$dotenv->load();
-
 use DigitalOceanV2\Adapter\BuzzAdapter;
 use DigitalOceanV2\DigitalOceanV2;
 
