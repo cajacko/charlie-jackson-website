d42344b4757f3e4ebf47c384347681b49495247b
Charlie Jackson <charlie@charliejackson.com>
2017-01-06 23:28:52 +0000

    Added a social menu

--- content/themes/charliejackson/template-parts/sections/social.php
+++ content/themes/charliejackson/template-parts/sections/social.php
@@ -1,11 +1,9 @@
 <section>
   <h2>Social</h2>
-  <ul>
-    <li><a>Twitter</a></li>
-    <li><a>LinkedIn</a></li>
-    <li><a>Pinterest</a></li>
-    <li><a>GitHub</a></li>
-    <li><a>Email</a></li>
-    <li><a>Email Newsletter</a></li>
-  </ul>
+  <?php
+  wp_nav_menu(array(
+    'menu' => 'social'
+  ));
+
+  ?>
 </section>
