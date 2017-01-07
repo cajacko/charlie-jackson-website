8be22d06993d113b4e0d22e598b69f138e3db0db
Charlie Jackson <contact@charliejackson.com>
2017-01-07 18:16:08 +0000

    Added pagination

--- content/themes/charliejackson/template-parts/sections/post-loop.php
+++ content/themes/charliejackson/template-parts/sections/post-loop.php
@@ -1,19 +1,28 @@
-<main>
+<div>
   <h2>Projects</h2>
-  <div>
-    <?php
-    if ( have_posts() ) :
+  <?php
+  if ( have_posts() ) : ?>
 
+    <main>
+      <?php
       while ( have_posts() ) : the_post();
 
         get_template_part( 'template-parts/post/post-loop-item' );
 
       endwhile;
-    else :
+      ?>
+    </main>
 
-      // get_template_part( 'template-parts/post/content', 'none' );
+    <?php
+    the_posts_pagination( array(
+      'prev_text' => '<span>Previous page</span>',
+      'next_text' => '<span>Next Page</span>'
+    ) );
 
-    endif;
-    ?>
-  </div>
-</main>
+  else :
+
+    // get_template_part( 'template-parts/post/content', 'none' );
+
+  endif;
+  ?>
+</div>
