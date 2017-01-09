4a51e6d1c78bd14626076f5c5dda96a516fc775e
Charlie Jackson <charlie@charliejackson.com>
2017-01-09 23:39:29 +0000

    Styled single project page

--- content/themes/charliejackson/template-parts/post/post-loop-item.php
+++ content/themes/charliejackson/template-parts/post/post-loop-item.php
@@ -1,21 +1,25 @@
-<article>
-  <?php if ('' !== get_the_post_thumbnail()): ?>
-		<div class="post-thumbnail">
-			<a href="<?php cj_the_post_permalink(); ?>">
-				<?php the_post_thumbnail(); ?>
-			</a>
-		</div>
-	<?php endif; ?>
+<article class="ProjectLoopItem">
+	<a class="ProjectLoopItem-imageLink" href="<?php cj_the_post_permalink(); ?>">
+    <?php if ('' !== get_the_post_thumbnail()): ?>
+			<?php the_post_thumbnail('thumbnail', array('class' => 'ProjectLoopItem-image')); ?>
+    <?php else: ?>
+      <img class="ProjectLoopItem-image" src="https://unsplash.it/200/200" />
+    <?php endif; ?>
+	</a>
 
-  <a href="<?php cj_the_post_permalink(); ?>"><?php the_title( '<h3>', '</h3>' ); ?></a>
-  <div>
-    <p>Last Updated: <?php the_date(); ?></p>
+  <div class="ProjectLoopItem-text">
+    <a class="ProjectLoopItem-titleLink" href="<?php cj_the_post_permalink(); ?>">
+      <?php the_title( '<h3 class="ProjectLoopItem-title">', '</h3>' ); ?>
+    </a>
+
+    <div class="ProjectLoopItem-meta">
+      <p class="ProjectLoopItem-date">Last Updated: <?php the_date(); ?></p>
+    </div>
+
+    <div class="ProjectLoopItem-content">
+      <?php the_excerpt(); ?>
+    </div>
+
+    <a class="ProjectLoopItem-readMore" href="<?php cj_the_post_permalink(); ?>">Read More</a>
   </div>
-
-  <div>
-    <?php the_excerpt(); ?>
-  </div>
-
-  <a href="<?php cj_the_post_permalink(); ?>">Read More</a>
-
 </article>
--- content/themes/charliejackson/template-parts/post/single-post.php
+++ content/themes/charliejackson/template-parts/post/single-post.php
@@ -1,16 +1,15 @@
-<article>
-  <?php the_title( '<h2>', '</h2>' ); ?>
-  <div>
-    <p>Last Updated: <?php the_date(); ?></p>
+<article class="Project">
+  <?php the_title( '<h2 class="Project-title">', '</h2>' ); ?>
+
+  <div class="Project-meta">
+    <p class="Project-date">Last Updated: <?php the_date(); ?></p>
   </div>
 
   <?php if ('' !== get_the_post_thumbnail()): ?>
-		<div class="post-thumbnail">
-			<?php the_post_thumbnail(); ?>
-		</div>
+			<?php the_post_thumbnail('large', array('class' => 'Project-image')); ?>
 	<?php endif; ?>
 
-  <div>
+  <div class="Project-content">
     <?php the_content(); ?>
   </div>
 </article>
