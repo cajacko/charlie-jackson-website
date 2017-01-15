268d3d60ae09289eba865f2e9fe4772c1ac8d588
Charlie Jackson <contact@charliejackson.com>
2017-01-15 12:50:02 +0000

    Added profile pictures to the now section

--- content/themes/charliejackson/template-parts/sections/now.php
+++ content/themes/charliejackson/template-parts/sections/now.php
@@ -3,8 +3,8 @@
   <?php echo apply_filters('the_content', get_option(NOW_NAME)); ?>
 
   <ul class="Now-imageList">
-    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="https://unsplash.it/200/200" /></a></li>
-    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="https://unsplash.it/200/200" /></a></li>
-    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="https://unsplash.it/200/200" /></a></li>
+    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-wedding.jpg" /></a></li>
+    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-wood-party.jpg" /></a></li>
+    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-beard.jpg" /></a></li>
   </ul>
 </section>
