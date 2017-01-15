beb545b858b52b7c80cb2a21eeb7d694bf478c6f
Charlie Jackson <contact@charliejackson.com>
2017-01-15 12:53:14 +0000

    Removed links from now section images

--- content/themes/charliejackson/template-parts/sections/now.php
+++ content/themes/charliejackson/template-parts/sections/now.php
@@ -3,8 +3,9 @@
   <?php echo apply_filters('the_content', get_option(NOW_NAME)); ?>
 
   <ul class="Now-imageList">
-    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-wedding.jpg" /></a></li>
-    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-wood-party.jpg" /></a></li>
-    <li class="Now-imageListItem"><a class="Now-imageLink"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-beard.jpg" /></a></li>
+    <li class="Now-imageListItem"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-wedding.jpg" /></li>
+    <li class="Now-imageListItem"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-wood-party.jpg" /></li>
+    <li class="Now-imageListItem"><img class="Now-image" src="/content/themes/charliejackson/media/charlie-jackson-profile-picture-beard.jpg" /></li>
   </ul>
 </section>
+  
