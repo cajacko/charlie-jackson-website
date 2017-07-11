d44bd0fdbc4157043dd8eb6de96e90e7197bbada
Charlie Jackson <contact@charliejackson.com>
2017-07-11 15:58:12 +0100

    Added contentful mapping for now component

--- src/components/Now/Now.jsx
+++ src/components/Now/Now.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
+import Image from 'components/Image/Image';
+import Item from 'containers/Item/Item';
 
-const SiteHeader = () => (
+const Now = ({ fields }) => (
   <section className="Now">
-    <h2 className="Now-title">Now</h2>
-    <p>I am currently a UX developer at <a href="https://charliejackson.com/readingroom/" target="_blank" rel="noopener noreferrer">Reading Room</a>, a web development agency based in London. Working a lot with React JS, Node JS, Sass and for big corporate and consumer websites.</p>
-    <p>I am living in Islington, London at the moment and doing a lot of improvised comedy all around the place and with the improv comedy group <a href="https://www.facebook.com/SuperCouth" target="_blank" rel="noopener noreferrer">SuperCouth</a>.</p>
-    <p>I&nbsp;do a lot of personal projects, all documented here on the site. My projects normally float around the areas of new tech startup ideas, apps, websites, art, music, improv videos, smart home technology and life hacking.</p>
-    <p>I’m also married to the wonderful&nbsp;blogger, artist and social media guru&nbsp;<a href="https://vikibell.com" target="_blank" rel="noopener noreferrer">Viki Bell</a>.</p>
+    <h2 className="Now-title">{fields.title['en-GB']}</h2>
+    <ReactMarkdown source={fields.content['en-GB']} />
 
     <ul className="Now-imageList">
-      <li className="Now-imageListItem"><img className="Now-image" alt="yay for now" src="https://charliejackson.com//content/themes/charliejackson/media/charlie-jackson-profile-picture-wedding.jpg" /></li>
-      <li className="Now-imageListItem"><img className="Now-image" alt="yay for now" src="https://charliejackson.com//content/themes/charliejackson/media/charlie-jackson-profile-picture-wood-party.jpg" /></li>
-      <li className="Now-imageListItem"><img className="Now-image" alt="yay for now" src="https://charliejackson.com//content/themes/charliejackson/media/charlie-jackson-profile-picture-beard.jpg" /></li>
+      {
+        fields.images['en-GB'].map(({ sys }) => (
+          <li key={sys.id} className="Now-imageListItem">
+            <Item itemId={sys.id} asset element={Image} />
+          </li>
+        ))
+      }
     </ul>
   </section>
 );
 
-export default SiteHeader;
+Now.propTypes = {
+  fields: PropTypes.shape({
+    title: PropTypes.shape({
+      'en-GB': PropTypes.string,
+    }),
+    content: PropTypes.shape({
+      'en-GB': PropTypes.string,
+    }),
+    images: PropTypes.shape({
+      'en-GB': PropTypes.arrayOf(PropTypes.shape({
+        sys: PropTypes.shape({
+          id: PropTypes.string,
+        }),
+      })),
+    }),
+  }).isRequired,
+};
+
+export default Now;
