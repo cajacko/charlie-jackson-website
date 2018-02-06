ef9c5d496c90304c4fd60b083041046997882851
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-06 17:52:54 +0000

    Initialised SiteNav

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SiteNav from '../SiteNav';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SiteNav />
+      </div>
+    );
+  }
+}
+
+export default App;
