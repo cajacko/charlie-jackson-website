b5bd26f52b883a5e56442f331a7f217635828cdb
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-07 08:11:54 +0000

    Added horizontal list

--- src/components/SocialIcons/index.js
+++ src/components/SocialIcons/index.js
@@ -1,19 +1,19 @@
 import React, { PureComponent } from 'react';
 import icons from './icons';
 import SocialIcon from '../SocialIcon';
+import HorizontalList from '../Lists/HorizontalList';
 
 class SocialIcons extends PureComponent {
   render() {
+    if (!icons || !icons.length) return null;
+
     return (
-      <ul>
-        {!!icons &&
-          !!icons.length &&
-          icons.map(({ icon, url }) => (
-            <li key={icon}>
-              <SocialIcon href={url} icon={icon} />
-            </li>
-          ))}
-      </ul>
+      <HorizontalList
+        list={icons.map(({ icon, url }) => ({
+          key: icon,
+          component: <SocialIcon href={url} icon={icon} />,
+        }))}
+      />
     );
   }
 }
