ecc196dff11568c304a16be43d7da30333f22dd6
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-07 20:18:24 +0000

    Improved icon slider nav item text hover styles

--- src/components/IconSlider/IconSliderNavItem/index.js
+++ src/components/IconSlider/IconSliderNavItem/index.js
@@ -3,22 +3,31 @@ import Text from '../../Text';
 import Icon from '../../Icon';
 import Button from '../../Buttons/Button';
 import SpacingContainer from '../../Containers/SpacingContainer';
+import './IconSliderNavItem.css';
 
 class IconSliderNavItem extends Component {
   render() {
-    return (
-      <Button>
+    const Content = (
+      <SpacingContainer pb>
         <div>
           <Icon
             icon={this.props.icon}
             color={this.props.active ? 'BLUE' : 'ORANGE'}
           />
           <SpacingContainer mt>
-            <Text text={this.props.label} />
+            <div className="iconslidernavitem__text">
+              <Text text={this.props.label} />
+            </div>
           </SpacingContainer>
         </div>
-      </Button>
+      </SpacingContainer>
     );
+
+    if (this.props.active) {
+      return <div>{Content}</div>;
+    }
+
+    return <Button>{Content}</Button>;
   }
 }
 
