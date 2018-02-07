829b5ea38f37be5c1652ed045de29feb8a032a10
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-07 17:13:15 +0000

    Grow icons and buttons on hover

--- src/components/SocialIcons/icons.js
+++ src/components/SocialIcons/icons.js
@@ -4,19 +4,19 @@ const icons = [
     url: 'https://twitter.com/charliejackson',
   },
   {
-    icon: 'instagram',
-    url: 'https://twitter.com/charliejackson',
-  },
-  {
-    icon: 'github',
-    url: 'https://twitter.com/charliejackson',
-  },
-  {
     icon: 'linkedin',
     url: 'https://twitter.com/charliejackson',
   },
   {
+    icon: 'instagram',
+    url: 'https://twitter.com/charliejackson',
+  },
+  {
     icon: 'youtube',
+    url: 'https://twitter.com/charliejackson',
+  },
+  {
+    icon: 'github',
     url: 'https://twitter.com/charliejackson',
   },
 ];
