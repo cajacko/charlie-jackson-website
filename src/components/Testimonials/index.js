fbf4a0b7384832683dd1a6fa09eec2e791298ea6
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-10 14:31:03 +0000

    Renaming icon slider

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,10 +1,13 @@
 import React, { PureComponent } from 'react';
 import ContentSpotlight from '../ContentSpotlight';
+import SliderContent from '../Slider/SliderContent';
 
 class Work extends PureComponent {
   render() {
     return (
-      <ContentSpotlight title="Some may say">Testimonials</ContentSpotlight>
+      <ContentSpotlight title="Some may say">
+        <SliderContent />
+      </ContentSpotlight>
     );
   }
 }
