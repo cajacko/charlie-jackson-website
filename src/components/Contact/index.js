c52238702a37ab7728423e3364d1a8744ec29e53
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-11 12:12:48 +0000

    Contact form html

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,20 @@
 import React, { PureComponent } from 'react';
 import ContentSpotlight from '../ContentSpotlight';
+import TextButton from '../Buttons/TextButton';
 
 class Contact extends PureComponent {
   render() {
     return (
       <ContentSpotlight title="Contact" theme="DARK">
-        Contact
+        <form>
+          <div>
+            <input placeholder="Name" type="text" />
+            <input placeholder="Email" type="text" />
+          </div>
+          <textarea placeholder="Message" />
+
+          <TextButton text="Submit" />
+        </form>
       </ContentSpotlight>
     );
   }
