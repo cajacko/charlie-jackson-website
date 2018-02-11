3fe4c6a283a1ea7e54bdff63c9376298e77b5b91
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-11 10:07:27 +0000

    Align skills columns

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,35 +3,47 @@ import ContentSpotlight from '../ContentSpotlight';
 import './Skills.css';
 import { about, skills } from './skills';
 import Text from '../Text';
+import SpacingContainer from '../Containers/SpacingContainer';
+import Paragraph from '../Paragraph';
 
 class Skills extends PureComponent {
   render() {
     return (
       <ContentSpotlight title="Me and my skills" theme="LIGHT">
         <div className="skills">
-          <div>
-            <Text text={about} />
+          <div className="skills__column">
+            <SpacingContainer mr2>
+              <div className="skills__content">
+                <Paragraph noSpacing>
+                  <Text text={about} />
+                </Paragraph>
+              </div>
+            </SpacingContainer>
           </div>
-          <div>
-            <table>
-              <tbody>
-                {skills.map(({ label, ...skill }) => (
-                  <tr key={label}>
-                    <td>
-                      <Text text={label} />
-                    </td>
-                    <td>
-                      {skill.skills.map((skillItem, i) => (
-                        <span key={skillItem}>
-                          {!!i && <Text text=", " />}
-                          <Text text={skillItem} />
-                        </span>
-                      ))}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <div className="skills__column">
+            <SpacingContainer ml2>
+              <div className="skills__content">
+                <table>
+                  <tbody>
+                    {skills.map(({ label, ...skill }) => (
+                      <tr key={label}>
+                        <td>
+                          <Text text={label} />
+                        </td>
+                        <td>
+                          {skill.skills.map((skillItem, i) => (
+                            <span key={skillItem}>
+                              {!!i && <Text text=", " />}
+                              <Text text={skillItem} />
+                            </span>
+                          ))}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </SpacingContainer>
           </div>
         </div>
       </ContentSpotlight>
