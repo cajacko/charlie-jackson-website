096da26a3102b4d1e1b839eff3d775a3b85aa9d1
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-11 09:56:41 +0000

    Added skills markup

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,12 +1,39 @@
 import React, { PureComponent } from 'react';
 import ContentSpotlight from '../ContentSpotlight';
 import './Skills.css';
+import { about, skills } from './skills';
+import Text from '../Text';
 
 class Skills extends PureComponent {
   render() {
     return (
       <ContentSpotlight title="Me and my skills" theme="LIGHT">
-        <div className="skills">About</div>
+        <div className="skills">
+          <div>
+            <Text text={about} />
+          </div>
+          <div>
+            <table>
+              <tbody>
+                {skills.map(({ label, ...skill }) => (
+                  <tr key={label}>
+                    <td>
+                      <Text text={label} />
+                    </td>
+                    <td>
+                      {skill.skills.map((skillItem, i) => (
+                        <span key={skillItem}>
+                          {!!i && <Text text=", " />}
+                          <Text text={skillItem} />
+                        </span>
+                      ))}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </ContentSpotlight>
     );
   }
