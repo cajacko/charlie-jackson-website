157ce5cfab0f8c4175911c0fe8731364b16a8199
Charlie Jackson <charlie@charliejackson.com>
2018-02-16 16:05:24 +0000

    Added images to services

--- src/constants/data.js
+++ src/constants/data.js
@@ -43,40 +43,40 @@ export const services = [
     icon: 'website',
     text:
       `Since 2012 I have been building websites for both large companies like Freeview to smaller startups. With experience building social media sites, e-commerce, brochureware and complex webapps.`,
-    image: require('../assets/countryside-alliance.png'),
-    imageAlt: 'Lorem ipsum dolor sit amet',
+    image: require('../assets/macbook.png'),
+    imageAlt: 'Website',
   },
   {
     label: 'Mobile Apps',
     icon: 'mobile',
     text:
       'I build apps that work on both iOS and Android devices. With live apps on both app stores, I can help bring your next idea to the market',
-    image: require('../assets/countryside-alliance.png'),
-    imageAlt: 'Lorem ipsum dolor sit amet',
+    image: require('../assets/mobile-apps.png'),
+    imageAlt: 'Mobile Apps',
   },
   {
     label: 'Desktop Apps',
     icon: 'desktop',
     text:
       'Mac, Windows, Linux? I can build your next app on all these platforms. I\'ve made everything from utility apps to complex photo manipulation products.',
-    image: require('../assets/countryside-alliance.png'),
-    imageAlt: 'Lorem ipsum dolor sit amet',
+    image: require('../assets/desktop.png'),
+    imageAlt: 'Desktop Apps',
   },
   {
     label: 'Startups',
     icon: 'startup',
     text:
       'I\'ve ran my own tech startup for 4 years, and been in and out of others every since. From crowdfunding campaigns to technical strategy I can help build your startup.',
-    image: require('../assets/countryside-alliance.png'),
-    imageAlt: 'Lorem ipsum dolor sit amet',
+    image: require('../assets/startup.png'),
+    imageAlt: 'Startup',
   },
   {
     label: 'Bespoke Solutions',
     icon: 'iot',
     text:
       'Need to automate social media, building an IoT device or maybe a integration with your Google services. I can build it, lets talk.',
-    image: require('../assets/countryside-alliance.png'),
-    imageAlt: 'Lorem ipsum dolor sit amet',
+    image: require('../assets/bespoke.png'),
+    imageAlt: 'Bespoke',
   },
 ];
 
