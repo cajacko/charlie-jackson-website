24eeaaf273b8387ee59f99ffd98148429b20f708
Charlie Jackson <charlie@charliejackson.com>
2018-02-19 16:44:14 +0000

    Action on contact icon

--- src/components/SocialIcon/index.js
+++ src/components/SocialIcon/index.js
@@ -1,30 +1,61 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Link from '../Link';
+import Button from '../Buttons/Button';
 import Icon from '../Icon';
 import SpacingContainer from '../Containers/SpacingContainer';
 import AlignContainer from '../Containers/AlignContainer';
 import './SocialIcon.css';
 
 class SocialIcon extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.action = this.action.bind(this);
+  }
+
+  action() {
+    this.context.showContactModal();
+  }
+
   render() {
-    return (
-      <div className={`socialicon ${this.props.isContact ? 'socialicon--contact' : ''}`}>
-      <Link href={this.props.href}>
-        <div
-          className={`socialicon__wrapper socialicon__wrapper--${this.props.icon} ${
-            this.props.light ? 'socialicon__wrapper--light' : 'socialicon__wrapper--color'
-          }`}
-        >
-          <SpacingContainer pr pl>
-            <AlignContainer vc>
-              <Icon icon={this.props.icon} size={this.props.size} />
-            </AlignContainer>
-          </SpacingContainer>
+    const content = (
+      <div
+        className={`socialicon__wrapper socialicon__wrapper--${
+          this.props.icon
+        } ${
+          this.props.light
+            ? 'socialicon__wrapper--light'
+            : 'socialicon__wrapper--color'
+        }`}
+      >
+        <SpacingContainer pr pl>
+          <AlignContainer vc>
+            <Icon icon={this.props.icon} size={this.props.size} />
+          </AlignContainer>
+        </SpacingContainer>
+      </div>
+    );
+
+    if (this.props.isContact) {
+      return (
+        <div className="socialicon socialicon--contact">
+          <Button action={this.action}>{content}</Button>
         </div>
-      </Link>
+      );
+    }
+
+    return (
+      <div className="socialicon">
+        <Link href={this.props.href}>{content}</Link>
       </div>
     );
   }
 }
+
+SocialIcon.contextTypes = {
+  showContactModal: PropTypes.func,
+};
+
 
 export default SocialIcon;
